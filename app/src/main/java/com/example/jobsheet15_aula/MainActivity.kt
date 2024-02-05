1fb7fbd811b4bc369105ccb053fb1de721673bd5
AulaNafisah <auladempet@gmail.com>
2024-02-05 15:26:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jobsheet15_aula/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jobsheet15_aula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"Aplikasi Berjalan", Toast.LENGTH_SHORT).show()
+
+        val nama = findViewById<Button>(R.id.tombolNama)
+        val nis = findViewById<Button>(R.id.tombolNis)
+        val kelas = findViewById<Button>(R.id.tombolKelas)
+
+        nama.setOnClickListener{
+            Toast.makeText(this,"Aula Nafisah", Toast.LENGTH_SHORT).show()
+        }
+        nis.setOnClickListener{
+            Toast.makeText(this,"2233280", Toast.LENGTH_SHORT).show()
+        }
+        kelas.setOnClickListener{
+            Toast.makeText(this,"XI PPLG 2", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
